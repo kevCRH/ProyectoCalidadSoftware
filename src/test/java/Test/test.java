6c6fe6f897be3541644850341ca151c1b57f3cd8
kevCRH <101358922+kevCRH@users.noreply.github.com>
2022-06-14 17:13:46 -0600

    Ambiente Selenium en Java.

---
+++ src/test/java/Test/test.java
@@ -0,0 +1,27 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class test {
+
+    public static void main(String[] args){
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Kevin\\Desktop\\Universidad\\2022\\7mo Cuatrimestre" +
+                "\\Calidad de software\\InstallationProject\\Proyecto\\untitled\\src\\main\\resources\\Drivers\\chromedriver.exe");
+
+        //initialize browser
+        WebDriver driver = new ChromeDriver();
+
+        // Open Youtube;
+        driver.get("https://www.youtube.com/");
+
+        //Maximize browser
+        driver.manage().window().maximize();
+
+        driver.close();
+
+    }
+
+
+}
+
