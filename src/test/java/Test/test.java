1a742b75b40503f2791d63d63a3197427e58f5c3
kevCRH <101358922+kevCRH@users.noreply.github.com>
2022-06-14 20:59:13 -0600

    Cambio de link de "youtube" a la pagina para el proyecto "ToolQA"

--- src/test/java/Test/test.java
+++ src/test/java/Test/test.java
@@ -2,23 +2,24 @@ package Test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 public class test {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\Kevin\\Desktop\\Universidad\\2022\\7mo Cuatrimestre" +
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kevin\\Desktop\\Universidad\\2022\\7mo Cuatrimestre" +
                 "\\Calidad de software\\InstallationProject\\Proyecto\\untitled\\src\\main\\resources\\Drivers\\chromedriver.exe");
 
         //initialize browser
         WebDriver driver = new ChromeDriver();
 
         // Open Youtube;
-        driver.get("https://www.youtube.com/");
+        driver.get("https://demoqa.com/");
 
         //Maximize browser
         driver.manage().window().maximize();
 
-        driver.close();
+        //driver.close();
 
     }
 
