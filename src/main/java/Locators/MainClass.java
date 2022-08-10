1bfcc0eb0ebe2c3f08c8278d3edae01641b44e95
kevCRH <101358922+kevCRH@users.noreply.github.com>
2022-08-09 20:11:37 -0600

    cambiamos el methodo main y le agregamos el InterruptedException al igual que en la clase de TestNG

--- src/main/java/Locators/MainClass.java
+++ src/main/java/Locators/MainClass.java
@@ -9,8 +9,8 @@ import Locators.TestNG;
 
 public class MainClass {
 
-    public static void main(String[] args) {
-        test t = new test();
+    public static void main(String[] args) throws InterruptedException {
+        //test t = new test();
         Test_1 l = new Test_1();
         Test_2 l2 = new Test_2();
         Test_3 l3 = new Test_3();
@@ -21,9 +21,9 @@ public class MainClass {
         //l.pruebaElements();
         //l2.pruebaWebTables();
         //l3.pruebaForms();
-        l4.pruebaDragDrop();
-        //l5.setBaseUrl();
-        //l5.testCase();
+        //l4.pruebaDragDrop();
+        l5.setBaseUrl();
+        l5.testCase();
 
     }
 }
--- src/main/java/Locators/TestNG.java
+++ src/main/java/Locators/TestNG.java
@@ -34,11 +34,13 @@ public class TestNG {
     }
 
     @Test
-    public void testCase() {
+    public void testCase() throws InterruptedException {
         String fullname = "Jorge Carranza Cespedes";
+
         String email = "ufyrCarranza@gmail.com";
         String currentAddress = "San Roque de Barva, Heredia.";
         String permanentAddress = "San Roque de Barva, Heredia.";
+        Thread.sleep(2500);
 
         //Localizamos por Xpath para las acciones de "Elements". En la opcion de "text Box"
         WebElement elementButton = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[1]/div/div[3]/h5"));
@@ -51,15 +53,21 @@ public class TestNG {
         WebElement emailInput = driver.findElement(By.id("userEmail"));
         WebElement currentAddressInput = driver.findElement(By.id("currentAddress"));
         WebElement permanentAddressInput = driver.findElement(By.id("permanentAddress"));
-
+        Thread.sleep(2500);
         // les mandamos la informacion con la que se desea llenar los espacios
         fullNameInput.sendKeys(fullname);
+        Thread.sleep(2500);
         emailInput.sendKeys(email);
+        Thread.sleep(2500);
         currentAddressInput.sendKeys(currentAddress);
+        Thread.sleep(2500);
         permanentAddressInput.sendKeys(permanentAddress);
+        Thread.sleep(2500);
 
         //indentificamos por id el buton de submit y lo clickeamos
         WebElement submitButton = driver.findElement(By.id("submit"));
         submitButton.click();
+        Thread.sleep(2500);
+        driver.close();
     }
 }
